87f8ecb2d354cc41d5099a041c9c4bffd334dd51
YutaroUeno0108 <yutaroueno@uenoyuutarou-no-MacBook-Air.local>
2017-05-20 19:09:37 +0900

    add test

---
+++ src/test/java/sample/employees/EmployeeControllerTest.java
@@ -0,0 +1,45 @@
+package sample.employees;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+/**
+ * Created by yutaroueno on 2017/05/20.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath:spring.xml" })
+public class EmployeeControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void slashへのGET() throws Exception {
+        mockMvc.perform(get("/employee-spring")).andExpect(status().isOk())
+                .andExpect(view().name("list-employees-spring"))
+                .andExpect(model().hasNoErrors());
+    }
+
+}
